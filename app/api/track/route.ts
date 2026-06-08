8167f7d0b2bacf2163e0f76b63b350b5a86c98f2
Paulo Assis <141682203+phpower1@users.noreply.github.com>
2026-06-08 00:50:17 -0400

    Delete app/api/track/route.ts

--- app/api/track/route.ts
+++
@@ -1,9 +0,0 @@
-export async function POST(request: Request) {
-  const body = await request.json();
-
-  console.log('GovIntel Tracking', body);
-
-  return Response.json({
-    success: true
-  });
-}
